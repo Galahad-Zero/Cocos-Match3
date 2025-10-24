70c788ef6d7d6849269d473b50493f8e012fecee
Galahad-Zero <galahad0425@gmail.com>
2025-10-24 10:44:24 +0800

    feat: 构建包

---
+++ build/web-desktop/src/settings.1da54.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Menu.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        main: "5a8b6"
+    }
+};
