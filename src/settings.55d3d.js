2a417bd1fc5716bad7b8bf1717cc75cbbbefceef
Galahad-Zero <Galahad-Zero@users.noreply.github.com>
2025-10-25 08:14:29 +0000

    deploy: a27460fbdd709af4fb6606c085c8f416e82567dd

---
+++ src/settings.55d3d.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Menu.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "d36c4",
+        main: "f8c66"
+    }
+};
